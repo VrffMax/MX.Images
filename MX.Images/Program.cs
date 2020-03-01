3be3c2f54dc5c0a5415d3b36678c3801f48d8918
Maxim V <vrffmax@hotmail.com>
2020-03-01 11:10:50 +0300

    Path validate

--- MX.Images/Program.cs
+++ MX.Images/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
@@ -14,140 +15,158 @@ using MX.Images.Models;
 
 namespace MX.Images
 {
-	public static class Program
-	{
-		private const string ResultPrefix = "*** Result ***";
+    public static class Program
+    {
+        private const string ResultPrefix = "*** Result ***";
 
-		private const string MxImage = "MX.Image";
-		private const string SourcePath = "[Source path]";
-		private const string DestinationPath = "[Destination path]";
+        private const string MxImage = "MX.Image";
+        private const string SourcePath = "[Source path]";
+        private const string DestinationPath = "[Destination path]";
 
-		private static readonly Dictionary<CommandEnum, string> CommandHelp = new Dictionary<CommandEnum, string>
-		{
-			{CommandEnum.Help, $"{MxImage} {CommandEnum.Help}"},
-			{CommandEnum.Scan, $"{MxImage} {CommandEnum.Scan} {SourcePath}"},
-			{CommandEnum.Sync, $"{MxImage} {CommandEnum.Sync} {SourcePath} {DestinationPath}"},
-			{CommandEnum.ScanSync, $"{MxImage} {CommandEnum.ScanSync} {SourcePath} {DestinationPath}"},
-			{CommandEnum.Verify, $"{MxImage} {CommandEnum.Verify} {SourcePath}"}
-		};
+        private static readonly Dictionary<CommandEnum, string> CommandHelp = new Dictionary<CommandEnum, string>
+        {
+            {CommandEnum.Help, $"{MxImage} {CommandEnum.Help}"},
+            {CommandEnum.Scan, $"{MxImage} {CommandEnum.Scan} {SourcePath}"},
+            {CommandEnum.Sync, $"{MxImage} {CommandEnum.Sync} {SourcePath} {DestinationPath}"},
+            {CommandEnum.ScanSync, $"{MxImage} {CommandEnum.ScanSync} {SourcePath} {DestinationPath}"},
+            {CommandEnum.Verify, $"{MxImage} {CommandEnum.Verify} {SourcePath}"}
+        };
 
-		private static readonly IMediator Mediator;
-		private static readonly IState State;
+        private static readonly IMediator Mediator;
+        private static readonly IState State;
 
-		static Program()
-		{
-			var builder = new ContainerBuilder();
+        static Program()
+        {
+            var builder = new ContainerBuilder();
 
-			builder.RegisterInstance(new Options()).As<IOptions>();
-			builder.RegisterInstance(new State()).As<IState>();
-			builder.RegisterType<Storage>().As<IStorage>();
-			builder.AddMediatR(typeof(Program).Assembly);
+            builder.RegisterInstance(new Options()).As<IOptions>();
+            builder.RegisterInstance(new State()).As<IState>();
+            builder.RegisterType<Storage>().As<IStorage>();
+            builder.AddMediatR(typeof(Program).Assembly);
 
-			var container = builder.Build();
+            var container = builder.Build();
 
-			Mediator = container.Resolve<IMediator>();
-			State = container.Resolve<IState>();
-		}
+            Mediator = container.Resolve<IMediator>();
+            State = container.Resolve<IState>();
+        }
 
-		private static async Task Main(string[] args)
-		{
-			var ticks = Environment.TickCount64;
-			{
-				await Handle(args);
+        private static async Task Main(string[] args)
+        {
+            var ticks = Environment.TickCount64;
+            {
+                await Handle(args);
 
-				Console.WriteLine(ResultPrefix);
-				foreach (var message in State.Messages) Console.WriteLine(message);
-			}
-			Console.WriteLine($"Ticks\t{Environment.TickCount64 - ticks}");
-		}
+                Console.WriteLine(ResultPrefix);
+                foreach (var message in State.Messages) Console.WriteLine(message);
+            }
+            Console.WriteLine($"Ticks\t{Environment.TickCount64 - ticks}");
+        }
 
-		private static Task Handle(string[] args)
-		{
-			return new CommandLine(args) switch
-			{
-				var (command, sourcePath)
-				when command == CommandEnum.Scan => Mediator.Send(new ScanCommand(sourcePath)),
+        private static Task Handle(string[] args)
+        {
+            return new CommandLine(args) switch
+            {
+                var (command, sourcePath)
+                when command == CommandEnum.Scan && sourcePath.IsPath() =>
+                Mediator.Send(new ScanCommand(sourcePath)),
 
-				var (command, sourcePath, destinationPath)
-				when command == CommandEnum.Sync => Mediator.Send(new SyncCommand(sourcePath, destinationPath)),
+                var (command, sourcePath, destinationPath)
+                when command == CommandEnum.Sync && sourcePath.IsPath() && destinationPath.IsPath() =>
+                Mediator.Send(new SyncCommand(sourcePath, destinationPath)),
 
-				var (command, sourcePath, destinationPath)
-				when command == CommandEnum.ScanSync => ScanSync(Mediator, sourcePath, destinationPath),
+                var (command, sourcePath, destinationPath)
+                when command == CommandEnum.ScanSync && sourcePath.IsPath() && destinationPath.IsPath() =>
+                ScanSync(Mediator, sourcePath, destinationPath),
 
-				var (command, sourcePath)
-				when command == CommandEnum.Verify => Mediator.Send(new VerifyCommand(sourcePath)),
+                var (command, sourcePath)
+                when command == CommandEnum.Verify && sourcePath.IsPath() =>
+                Mediator.Send(new VerifyCommand(sourcePath)),
 
-				var (command)
-				when command != CommandEnum.Help => command.Help(),
+                var (command)
+                when command != CommandEnum.Help => command.Help(),
 
-				_ => Help()
-			};
-		}
+                _ => Help()
+            };
+        }
 
-		private static async Task<Unit> ScanSync(IMediator mediator, string sourcePath, string destinationPath)
-		{
-			await mediator.Send(new ScanCommand(sourcePath));
-			await mediator.Send(new SyncCommand(sourcePath, destinationPath));
-			return Unit.Value;
-		}
+        private static async Task<Unit> ScanSync(IMediator mediator, string sourcePath, string destinationPath)
+        {
+            await mediator.Send(new ScanCommand(sourcePath));
+            await mediator.Send(new SyncCommand(sourcePath, destinationPath));
+            return Unit.Value;
+        }
 
-		private static Task<Unit> Help()
-		{
-			Console.WriteLine(string.Join(Environment.NewLine, CommandHelp.Values.Select(help => help)));
-			return Task.FromResult(Unit.Value);
-		}
+        private static Task<Unit> Help()
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, CommandHelp.Values.Select(help => help)));
+            return Task.FromResult(Unit.Value);
+        }
 
-		private static Task<Unit> Help(this CommandEnum command)
-		{
-			Console.WriteLine(CommandHelp[command]);
-			return Task.FromResult(Unit.Value);
-		}
+        private static Task<Unit> Help(this CommandEnum command)
+        {
+            Console.WriteLine(CommandHelp[command]);
+            return Task.FromResult(Unit.Value);
+        }
 
-		private enum CommandEnum
-		{
-			Help,
-			Scan,
-			Sync,
-			ScanSync,
-			Verify
-		}
+        private static bool IsPath(this string path)
+        {
+            try
+            {
+                var _ = new FileInfo(path);
+            }
+            catch
+            {
+                return false;
+            }
 
-		private class CommandLine
-		{
-			private readonly CommandEnum _command = CommandEnum.Help;
-			private readonly string _destinationPath;
+            return true;
+        }
 
-			private readonly string _sourcePath;
+        private enum CommandEnum
+        {
+            Help,
+            Scan,
+            Sync,
+            ScanSync,
+            Verify
+        }
 
-			public CommandLine(string[] args)
-			{
-				var argsQueue = new Queue<string>(args);
+        private class CommandLine
+        {
+            private readonly CommandEnum _command = CommandEnum.Help;
+            private readonly string _destinationPath;
 
-				if (argsQueue.Any() && Enum.TryParse<CommandEnum>(argsQueue.Dequeue(), true, out var command))
-					_command = command;
+            private readonly string _sourcePath;
 
-				if (argsQueue.Any()) _sourcePath = argsQueue.Dequeue();
+            public CommandLine(string[] args)
+            {
+                var argsQueue = new Queue<string>(args);
 
-				if (argsQueue.Any()) _destinationPath = argsQueue.Dequeue();
-			}
+                if (argsQueue.Any() && Enum.TryParse<CommandEnum>(argsQueue.Dequeue(), true, out var command))
+                    _command = command;
 
-			public void Deconstruct(out CommandEnum command)
-			{
-				command = _command;
-			}
+                if (argsQueue.Any()) _sourcePath = argsQueue.Dequeue();
 
-			public void Deconstruct(out CommandEnum command, out string sourcePath)
-			{
-				command = _command;
-				sourcePath = _sourcePath;
-			}
+                if (argsQueue.Any()) _destinationPath = argsQueue.Dequeue();
+            }
 
-			public void Deconstruct(out CommandEnum command, out string sourcePath, out string destinationPath)
-			{
-				command = _command;
-				sourcePath = _sourcePath;
-				destinationPath = _destinationPath;
-			}
-		}
-	}
+            public void Deconstruct(out CommandEnum command)
+            {
+                command = _command;
+            }
+
+            public void Deconstruct(out CommandEnum command, out string sourcePath)
+            {
+                command = _command;
+                sourcePath = _sourcePath;
+            }
+
+            public void Deconstruct(out CommandEnum command, out string sourcePath, out string destinationPath)
+            {
+                command = _command;
+                sourcePath = _sourcePath;
+                destinationPath = _destinationPath;
+            }
+        }
+    }
 }
